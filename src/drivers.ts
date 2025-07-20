dcb3709fe18ea2b58561f3d30a36117c09baa7a9
Julien Ripouteau <julien@ripouteau.com>
2025-07-21 01:46:14 +0200

    fix: re-add missing prefix

--- src/drivers.ts
+++ src/drivers.ts
@@ -59,7 +59,10 @@ export const drivers: {
       const { redisDriver } = await import('bentocache/drivers/redis')
 
       const redisConnection = redis.connection(config.connectionName) as any as RedisConnection
-      return redisDriver({ connection: redisConnection.ioConnection, prefix: config.prefix })
+      return redisDriver({
+        connection: redisConnection.ioConnection,
+        prefix: config.prefix || 'bentocache',
+      })
     })
   },
 
@@ -116,7 +119,7 @@ export const drivers: {
         autoCreateTable: config?.autoCreateTable ?? true,
         tableName: config?.tableName || 'bentocache',
         pruneInterval: config?.pruneInterval ?? false,
-        prefix: config?.prefix,
+        prefix: config?.prefix || 'bentocache',
       })
     })
   },
